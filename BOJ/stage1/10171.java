fce72065130fb0fab4eec585e12d9b48109a4047
liz-stack <yeoju.kook@gmail.com>
2022-02-02 23:08:26 +0900

    1단계 사칙연산까지 완료

---
+++ BOJ/stage1/10171.java
@@ -0,0 +1,11 @@
+package stage1;
+
+public class BOJ10171 {
+
+	public static void main(String[] args) {
+		System.out.print("\\    /\\\n");
+		System.out.print(" )  ( \')\n");
+		System.out.print("(  /  )\n");
+		System.out.println(" \\(__)|");
+	}
+}
