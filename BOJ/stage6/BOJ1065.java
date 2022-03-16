c3c814bfdc51f9fdc31223b88d1d858c4aaee155
liz-stack <yeoju.kook@gmail.com>
2022-03-16 23:44:07 +0900

    BOJ1065 한수

---
+++ BOJ/stage6/BOJ1065.java
@@ -0,0 +1,43 @@
+package stage6;
+
+import java.util.Scanner;
+
+public class BOJ1065 {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print(seq(sc.nextInt()));;
+	sc.close();
+		
+	}
+
+	public static int seq(int num) {
+		int count = 0;
+
+		if (num < 100) {
+			return num;
+		}
+		
+		else {
+			count = 99;
+			if (num == 1000) {
+				num = 999;
+			}
+
+			for (int i = 100; i <= num; i++) {
+				int hun = i / 100;
+				int ten = (i / 10) % 10;
+				int one = i % 10;
+
+				if ((hun - ten) == (ten - one)) {
+					count++;
+				}
+			}
+
+		}
+
+		return count;
+
+	}
+
+}
