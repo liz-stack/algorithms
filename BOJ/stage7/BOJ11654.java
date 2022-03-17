edbc3b2cc511ba241c0dbaecc0b6a1908502aad5
liz-stack <yeoju.kook@gmail.com>
2022-03-17 23:26:22 +0900

    11654(아스키코드), 11720(숫자의합)

---
+++ BOJ/stage7/BOJ11654.java
@@ -0,0 +1,12 @@
+package stage7;
+
+//아스키코드
+public class BOJ11654 {
+	public static void main(String[] args) throws Exception {
+		
+		int a = System.in.read();
+		
+		System.out.print(a);
+	}
+
+}
